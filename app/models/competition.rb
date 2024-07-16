ed98a2ec0643762844a53ebce312a52fd485ff07
Gregor Billing <gbilling@worldcubeassociation.org>
2024-07-16 17:34:34 +0200

    Relax competition search by including all venues

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -81,12 +81,12 @@ class Competition < ApplicationRecord
   scope :between_dates, ->(start_date, end_date) { where("start_date <= ? AND end_date >= ?", end_date, start_date) }
   scope :end_date_passed_since, ->(num_days) { where(end_date: ...(num_days.days.ago)) }
   scope :belongs_to_region, lambda { |region_id|
-    joins(main_venue: [:country]).where(
+    joins(competition_venues: [:country]).where(
       "Countries.id = :region_id OR Countries.continentId = :region_id", region_id: region_id
     )
   }
   scope :contains, lambda { |search_term|
-    joins(:main_venue)
+    joins(:competition_venues)
       .where(
         "competitions.name like :search_term or
         competition_venues.city like :search_term",
@@ -1645,16 +1645,16 @@ class Competition < ApplicationRecord
       continent = Continent.find(params[:continent])
       raise WcaExceptions::BadApiParameter.new("Invalid continent: '#{params[:continent]}'") unless continent
 
-      competitions = competitions.joins(main_venue: [:continent])
-                                 .where(main_venue: { continents: { id: continent } })
+      competitions = competitions.left_outer_joins(competition_venues: [:continent])
+                                 .where(competition_venues: { continents: { id: continent } })
     end
 
     if params[:country_iso2].present?
       country = Country.find_by(iso2: params[:country_iso2])
       raise WcaExceptions::BadApiParameter.new("Invalid country_iso2: '#{params[:country_iso2]}'") unless country
 
-      competitions = competitions.joins(:main_venue)
-                                 .where(main_venue: { countries: { id: country } })
+      competitions = competitions.left_outer_joins(competition_venues: [:country])
+                                 .where(competition_venues: { countries: { id: country } })
     end
 
     if params[:delegate].present?
@@ -1723,7 +1723,7 @@ class Competition < ApplicationRecord
       venue_like_query = %w[city].map { |column| "competition_venues.#{column} LIKE :part" }
       country_like_query = %w[id].map { |column| "Countries.#{column} LIKE :part" }
       like_query = (competitions_like_query + venue_like_query + country_like_query).join(" OR ")
-      competitions = competitions.joins(main_venue: [:country])
+      competitions = competitions.joins(competition_venues: [:country])
                                  .where(like_query, part: "%#{part}%")
     end
 
