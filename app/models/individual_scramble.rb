7cff484ef4776fbfccd673daf9c087f937083a5e
Gregor Billing <gbilling@worldcubeassociation.org>
2026-03-16 18:01:53 +0100

    Implement serialization layer for IndividualScramble

--- app/models/individual_scramble.rb
+++ app/models/individual_scramble.rb
@@ -3,5 +3,16 @@
 class IndividualScramble < ApplicationRecord
   self.table_name = "scrambles"
 
-  belongs_to :scramble
+  belongs_to :scramble, foreign_key: "scramble_set_id", inverse_of: :individual_scrambles
+
+  delegate :competition_id, :event_id, :round_type_id, :round_id, :group_id, to: :scramble
+
+  DEFAULT_SERIALIZE_OPTIONS = {
+    only: %w[id is_extra scramble_number scramble_string],
+    methods: %w[competition_id event_id round_type_id round_id group_id],
+  }.freeze
+
+  def serializable_hash(options = nil)
+    super(DEFAULT_SERIALIZE_OPTIONS.merge(options || {}))
+  end
 end
