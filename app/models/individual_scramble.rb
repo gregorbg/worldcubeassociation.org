ebe1a213b630fc9ff02db0ad8e91e2913970e552
Gregor Billing <gbilling@worldcubeassociation.org>
2026-03-17 06:24:34 +0100

    Improve backwards compatibility in serialization

--- app/models/individual_scramble.rb
+++ app/models/individual_scramble.rb
@@ -3,13 +3,16 @@
 class IndividualScramble < ApplicationRecord
   self.table_name = "scrambles"
 
-  belongs_to :scramble, foreign_key: "scramble_set_id", inverse_of: :individual_scrambles
+  belongs_to :scramble_set, class_name: "Scramble", inverse_of: :individual_scrambles
 
-  delegate :competition_id, :event_id, :round_type_id, :round_id, :group_id, to: :scramble
+  alias_attribute :scramble_num, :scramble_number
+  alias_attribute :scramble, :scramble_string
+
+  delegate :competition_id, :event_id, :round_type_id, :round_id, :group_id, to: :scramble_set
 
   DEFAULT_SERIALIZE_OPTIONS = {
-    only: %w[id is_extra scramble_number scramble_string],
-    methods: %w[competition_id event_id round_type_id round_id group_id],
+    only: %w[id is_extra],
+    methods: %w[competition_id event_id round_type_id round_id group_id scramble_num scramble],
   }.freeze
 
   def serializable_hash(options = nil)
