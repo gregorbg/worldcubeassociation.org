d0afcc41e8cba074476964efa56d3d3cb5e541c3
Gregor Billing <gbilling@worldcubeassociation.org>
2026-06-23 17:50:21 +0200

    Only retrofill stuff that hasn't been retrofilled

--- db/migrate/20240503161312_retrofill_competition_main_venues.rb
+++ db/migrate/20240503161312_retrofill_competition_main_venues.rb
@@ -2,55 +2,58 @@
 
 class RetrofillCompetitionMainVenues < ActiveRecord::Migration[7.1]
   def change
-    Competition.includes(:competition_venues).find_each do |competition|
-      venue_count = competition.competition_venues.count
+    Competition
+      .includes(:competition_venues)
+      .where(main_venue: nil)
+      .find_each do |competition|
+        venue_count = competition.competition_venues.count
 
-      if venue_count == 1
-        main_venue = competition.competition_venues.first
+        if venue_count == 1
+          main_venue = competition.competition_venues.first
 
-        venue_latitude = main_venue.latitude_microdegrees
-        venue_longitude = main_venue.longitude_microdegrees
+          venue_latitude = main_venue.latitude_microdegrees
+          venue_longitude = main_venue.longitude_microdegrees
 
-        guessed_timezone = TZF.tz_name(venue_latitude / 1e6, venue_longitude / 1e6)
-        best_timezone = main_venue.timezone_id.presence || guessed_timezone
+          guessed_timezone = TZF.tz_name(venue_latitude / 1e6, venue_longitude / 1e6)
+          best_timezone = main_venue.timezone_id.presence || guessed_timezone
 
-        main_venue.assign_attributes(
-          city: competition.attributes['city_name'],
-          address: competition.attributes['venue_address'],
-          description: competition.attributes['venue_details'],
-          timezone_id: best_timezone,
-        )
-      elsif venue_count.zero?
-        venue_country = Country.c_find!(competition.attributes['country_id'])
+          main_venue.assign_attributes(
+            city: competition.attributes['city_name'],
+            address: competition.attributes['venue_address'],
+            description: competition.attributes['venue_details'],
+            timezone_id: best_timezone,
+          )
+        elsif venue_count.zero?
+          venue_country = Country.c_find!(competition.attributes['country_id'])
 
-        timezone = 'Etc/UTC'
+          timezone = 'Etc/UTC'
 
-        comp_latitude = competition.attributes['latitude']
-        comp_longitude = competition.attributes['longitude']
+          comp_latitude = competition.attributes['latitude']
+          comp_longitude = competition.attributes['longitude']
 
-        if comp_latitude.present? && comp_longitude.present?
-          guessed_timezone = TZF.tz_name(comp_latitude / 1e6, comp_longitude / 1e6)
-          timezone = guessed_timezone || timezone
+          if comp_latitude.present? && comp_longitude.present?
+            guessed_timezone = TZF.tz_name(comp_latitude / 1e6, comp_longitude / 1e6)
+            timezone = guessed_timezone || timezone
+          end
+
+          main_venue = competition.competition_venues.build(
+            wcif_id: 1,
+            name: competition.attributes['venue'],
+            city: competition.attributes['city_name'],
+            address: competition.attributes['venue_address'],
+            description: competition.attributes['venue_details'],
+            latitude_microdegrees: comp_latitude,
+            longitude_microdegrees: comp_longitude,
+            country_iso2: venue_country.iso2,
+            timezone_id: timezone,
+          )
         end
 
-        main_venue = competition.competition_venues.build(
-          wcif_id: 1,
-          name: competition.attributes['venue'],
-          city: competition.attributes['city_name'],
-          address: competition.attributes['venue_address'],
-          description: competition.attributes['venue_details'],
-          latitude_microdegrees: comp_latitude,
-          longitude_microdegrees: comp_longitude,
-          country_iso2: venue_country.iso2,
-          timezone_id: timezone,
-        )
-      end
+        next if main_venue.blank?
 
-      next if main_venue.blank?
-
-      # Legacy data sometimes does not match modern WCIF conventions. Save anyways.
-      main_venue.save(validate: false)
-      competition.update_attribute(:main_venue, main_venue)
+        # Legacy data sometimes does not match modern WCIF conventions. Save anyways.
+        main_venue.save(validate: false)
+        competition.update_attribute(:main_venue, main_venue)
     end
   end
 end
