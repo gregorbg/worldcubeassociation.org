0bd9fa9cc90d5216942b21c9f8401ea872134d3a
Gregor Billing <gbilling@worldcubeassociation.org>
2026-07-28 11:37:03 +0200

    Add action_source enum to RegHistoryEntry

--- app/models/registration_history_entry.rb
+++ app/models/registration_history_entry.rb
@@ -3,4 +3,14 @@
 class RegistrationHistoryEntry < ApplicationRecord
   has_many :registration_history_changes, dependent: :destroy
   belongs_to :registration
+
+  enum :action_source, {
+    registration_form: 'registration-form',
+    admin_ui: 'admin-ui',
+    queue_worker: 'queue-worker',
+    live_auto_accept: 'live-auto-accept',
+    bulk_auto_accept: 'bulk-auto-accept',
+    ots_form: 'ots-form',
+    csv_import: 'csv-import',
+  }, prefix: true
 end
